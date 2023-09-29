47f550a2effc5faffa091852969c3b9cb31fa44a
Ana Carolina <acarolinamat@gmail.com>
2023-09-29 12:21:04 +0100

    Integrate Charts - Step 2

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,6 +1,38 @@
 import React from "react";
 import { Card, CardHeader, Text } from "@ui5/webcomponents-react";
 import { spacing } from "@ui5/webcomponents-react-base";
+import { BarChart, LineChart } from "@ui5/webcomponents-react-charts";
+
+const dataset = [
+    {
+      month: "January",
+      data: 65
+    },
+    {
+      month: "February",
+      data: 59
+    },
+    {
+      month: "March",
+      data: 80
+    },
+    {
+      month: "April",
+      data: 81
+    },
+    {
+      month: "May",
+      data: 56
+    },
+    {
+      month: "June",
+      data: 55
+    },
+    {
+      month: "July",
+      data: 40
+    }
+  ];
 
 export function MyApp() {
     const handleHeaderClick = () => {
@@ -19,6 +51,16 @@ export function MyApp() {
                 <Text style={ spacing.sapUiContentPadding }>
                     This is the area of the Card
                 </Text>
+                <LineChart 
+                    measures={[{ accessor: "data", label: "Stock Price" }]} 
+                    dimensions={[{ accessor: "month" }]} 
+                    dataset={dataset}
+                />
+                <BarChart 
+                    measures={[{ accessor: "data", label: "Stock Price" }]} 
+                    dimensions={[{ accessor: "month" }]} 
+                    dataset={dataset}
+                />
             </Card>
         </div>;
 }
