a16db0fa3cf46bf94c795473ff050bd64d887b5a
Ana Carolina <acarolinamat@gmail.com>
2023-09-29 12:02:17 +0100

    Create a Card Component

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { Card, CardHeader, Text } from "@ui5/webcomponents-react";
+import { spacing } from "@ui5/webcomponents-react-base";
 
 export function MyApp() {
-    return <div>My root Component</div>;
+    const handleHeaderClick = () => {
+        alert("Header clicked");
+    };
+    return <div>
+            <Card header={
+                <CardHeader 
+                titleText="Card" 
+                interactive 
+                onClick={handleHeaderClick}
+                />
+                } 
+                style={{ width: "300px" }}
+            >
+                <Text style={ spacing.sapUiContentPadding }>
+                    This is the area of the Card
+                </Text>
+            </Card>
+        </div>;
 }
